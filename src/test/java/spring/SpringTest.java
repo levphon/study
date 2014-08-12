104da0e5187b7ed7fb4aded6bc3c36bd8ff3ec99
hebaceous <hebaceous@gmail.com>
2014-08-12 13:06:55 +0800

    spring

---
+++ src/test/java/spring/SpringTest.java
@@ -0,0 +1,46 @@
+package spring;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/beans.xml")
+public class SpringTest {
+
+	@Autowired
+	private ApplicationContext context;
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/beans.xml");
+		System.out.println(context);
+	}
+
+	@Test
+	public void testApplicationContext() {
+		System.out.println(context);
+	}
+
+	@Test
+	public void testDataSource() throws SQLException {
+		DataSource dataSource = context.getBean("dataSource", DataSource.class);
+		System.out.println(dataSource.getConnection());
+	}
+
+	@Test
+	public void testJdbcTemplate() {
+		JdbcTemplate jdbcTemplate = context.getBean("jdbcTemplate", JdbcTemplate.class);
+		String sysdate = jdbcTemplate.queryForObject("select sysdate from dual", String.class);
+		System.out.println(sysdate);
+	}
+
+}
