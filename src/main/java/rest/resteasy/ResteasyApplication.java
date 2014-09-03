ffbca5824c85072a8637de3d96da5249180a44e3
Hebaceous <hebaceous@gmail.com>
2014-09-03 22:27:39 +0800

    ServiceLoader

--- src/main/java/rest/resteasy/ResteasyApplication.java
+++ src/main/java/rest/resteasy/ResteasyApplication.java
@@ -3,18 +3,25 @@ package rest.resteasy;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-import rest.resteasy.resource.bean.CustomerResourceBean;
+import rest.resteasy.resource.CustomerResource;
 
 import java.util.HashSet;
+import java.util.ServiceLoader;
 import java.util.Set;
 
 @ApplicationPath("/resteasy")
 public class ResteasyApplication extends Application {
 
+	/**
+	 * 注册各个resource实现类
+	 *  @see ServiceLoader
+	 */
+	private CustomerResource customerResource = ServiceLoader.load(CustomerResource.class).iterator().next();
+
 	private Set<Object> singletons = new HashSet<Object>();
 
 	public ResteasyApplication() {
-		singletons.add(new CustomerResourceBean());
+		singletons.add(customerResource);
 	}
 
 	@Override
